e1c8c1a666c7708a34b92df1bbe856d8d9d5061c
Abhilash Nair <abhilashnair1994@gmail.com>
2022-03-21 15:47:28 -0400

    remove src (#3)

---
+++ tests/test_secretsmanager.py
@@ -0,0 +1,14 @@
+from secretsmanager.secretsmanager import SecretsManager
+import unittest
+from unittest.mock import patch
+import pytest
+import sys
+
+
+def test_output(awssecrets_connection):
+    assert awssecrets_connection.region_name == 'us-west-2'
+    assert awssecrets_connection.db_secret.__contains__('host')
+    assert awssecrets_connection.db_secret.__contains__('username')
+    assert awssecrets_connection.db_secret.__contains__('password')
+    assert awssecrets_connection.db_secret.__contains__('port')
+    assert awssecrets_connection.db_secret.__contains__('dbname')
